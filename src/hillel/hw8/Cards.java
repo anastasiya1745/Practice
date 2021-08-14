2acdbf5ef2c94ee9c78998c64ed2460bde956668
anastasiya1745 <anastasiya.od@gmail.com>
2021-08-14 19:42:47 +0300

    create class Cards,PokerAction,Main

---
+++ src/hillel/hw8/Cards.java
@@ -0,0 +1,15 @@
+package hillel.hw8;
+
+public class Cards {
+    private String value;
+    private String suite;
+
+    public Cards(String value, String suite) {
+        this.value = value;
+        this.suite = suite;
+    }
+
+   public String toString() {
+        return "Card " + value + " of " + suite;
+   }
+}
