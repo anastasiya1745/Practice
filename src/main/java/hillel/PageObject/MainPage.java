3d5d4a1d3c336acf8233245462eca2b5d057b38a
anastasiya1745 <anastasiya.od@gmail.com>
2021-10-19 23:17:07 +0300

    update with attachment and steps

--- src/main/java/hillel/PageObject/MainPage.java
+++ src/main/java/hillel/PageObject/MainPage.java
@@ -1,5 +1,6 @@
 package hillel.PageObject;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -7,23 +8,33 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MainPage extends BasePage  {
 
+
+
+    public MainPage openApp(){
+        driver.get("https://the-internet.herokuapp.com");
+        return new MainPage(driver);
+    }
     public MainPage(WebDriver driver) {
+
         super(driver);
     }
 
-
+    @Step("Navigate to Hover Page")
     public HoverPage toNavHoverPage(){
         driver.findElement(By.linkText("Hovers")).click();
         return new HoverPage(driver);
     }
+    @Step("Navigate to GeoLocation Page")
     public GeoLocationPage toNavGeoLocationPage(){
         driver.findElement(By.linkText("Geolocation")).click();
         return new GeoLocationPage(driver);
     }
+    @Step("Navigate to Login Page")
     public LoginPage toNavLoginPage(){
         driver.findElement(By.linkText("Form Authentication")).click();
         return new LoginPage(driver);
     }
+    @Step("Navigate to сhallengeDom Page")
     public ChallengeDomPage toNavсhallengeDomPage(){
         driver.findElement(By.linkText("Challenging DOM")).click();
         return new ChallengeDomPage(driver);
--- src/main/java/hillel/PageObject/SecurePage.java
+++ src/main/java/hillel/PageObject/SecurePage.java
@@ -1,5 +1,6 @@
 package hillel.PageObject;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -8,14 +9,19 @@ public class SecurePage extends BasePage{
     public SecurePage(WebDriver driver){
         super(driver);
     }
+
+
+    @Step("Read success message")
     public SecurePage readMsgPostiveLogin(String message){
         driver.findElement(By.xpath("//div[@class='flash success']")).getText().contains(message);
         return this;
     }
+    @Step("Read error message")
     public void readMsgNegativeLogin(String message){
         driver.findElement(By.xpath("//div[@class='flash error']")).getText().contains(message);
 
     }
+    @Step("Click on Logout")
     public void clickOnLogout(){
         driver.findElement(By.xpath("//i[@class='icon-2x icon-signout']")).click();
 
