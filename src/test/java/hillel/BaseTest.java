f59184e00dc615238d4d9514decb51aaf1a9bd3c
anastasiya1745 <anastasiya.od@gmail.com>
2021-10-22 19:02:41 +0300

    update

--- src/test/java/hillel/BaseTest.java
+++ src/test/java/hillel/BaseTest.java
@@ -1,12 +1,9 @@
 package hillel;
-
-import hillel.PageObject.MainPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
